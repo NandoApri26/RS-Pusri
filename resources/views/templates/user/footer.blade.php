9307edaaef87563f0baecb436d5f77c82e06ca9e
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-28 16:04:29 +0700

    Added new footer template to user views. #0005

---
+++ resources/views/templates/user/footer.blade.php
@@ -0,0 +1,63 @@
+<footer class="bg-white pt-10 pb-4 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+    <div class="flex flex-col px-8 gap-4 md:flex-row md:mx-12 xl:mx-20 2xl:mx-80">
+        <div class="flex w-full justify-between md:justify-around">
+            <div class="space-y-8">
+                <h1 class="font-bold text-xl">TENTANG KAMI</h1>
+                <ul class="space-y-4">
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">PROFILE</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">VISI MISI</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">PEMEGANG SAHAM</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">STRUKTUR</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="space-y-8 text-right xl:text-left">
+                <h1 class="font-bold text-xl">LAYANAN</h1>
+                <ul class="space-y-4">
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">JADWAL</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">FASILITAS</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex w-full justify-between md:justify-around">
+            <div class="space-y-8">
+                <h1 class="font-bold text-xl">INFORMASI</h1>
+                <ul class="space-y-4">
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">BERITA</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">GALERI</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="space-y-8 text-right xl:text-left">
+                <h1 class="font-bold text-xl">HUBUNGI KAMI</h1>
+                <ul class="space-y-4">
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">TESTIMONI</a>
+                    </li>
+                    <li>
+                        <a href="" class="text-gray-600 font-semibold hover:text-sky-500">KONTAK</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="flex mt-6 cursor-pointer justify-center">
+        <h1 class="font-semibold text-lg">
+            © Copyright 2024. <span class="font-bold text-sky-600">RS Pusri</span>
+        </h1>
+    </div>
+</footer>

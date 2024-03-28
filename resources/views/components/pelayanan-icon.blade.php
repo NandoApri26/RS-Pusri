550ef6b75bd528f009b666ed98e339910975e114
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-28 16:05:21 +0700

    Add pelayanan-icon component to the project #0005

---
+++ resources/views/components/pelayanan-icon.blade.php
@@ -0,0 +1,15 @@
+@props([
+    'iconName' => '',
+    'classStyle' => '',
+    'widthStyle' => ''
+])
+<div class="flex flex-col items-center gap-6 w-[94px] lg:w-[130px] {{ $widthStyle }}">
+    <div {{ $attributes->merge([
+        'class' => 'flex p-6 bg-white shadow-md justify-center rounded-lg'
+    ]) }}>
+        {{ $slot }}
+    </div>
+    <div class="">
+        <h1 class="text-lg text-gray-600 font-bold text-center {{ $classStyle }}">{{ $iconName }}</h1>
+    </div>
+</div>

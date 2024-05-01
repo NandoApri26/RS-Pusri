b51e90474d5c83948fbe1618d967698778544e63
NandoApri26 <apriando.pratamaa@gmail.com>
2024-05-01 22:56:58 +0700

    Update controller usage and add new routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\landingpageController;
 use App\Http\Controllers\tentangkamiController;
-use App\Http\Controllers\jadwalController;
+use App\Http\Controllers\dokterController;
 use App\Http\Controllers\layananController;
 
 /*
@@ -29,9 +29,13 @@ Route::get('/used-component', function () {
 Route::get('/beranda', [landingpageController::class, 'index'])->name('beranda');
 Route::get('/tentang-kami', [tentangkamiController::class, 'index'])->name('tentang-kami');
 
+// Layanan
 Route::prefix('/layanan')->group(function () {
     Route::get('/jadwal', [layananController::class, 'jadwal'])->name('jadwal');
     Route::get('/rawat-jalan', [layananController::class, 'rawatJalan'])->name('rawat-jalan');
     Route::get('/rawat-inap', [layananController::class, 'rawatInap'])->name('rawat-inap');
     Route::get('/detail-ruangan', [layananController::class, 'detailRuangan'])->name('detail-ruangan');
 });
+
+// Doketer
+Route::get('/dokter', [dokterController::class, 'index'])->name('dokter');

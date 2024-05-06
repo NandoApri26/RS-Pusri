2698dbe1055a546de358380d2ce902bb5a314821
NandoApri26 <apriando.pratamaa@gmail.com>
2024-05-07 00:43:05 +0700

    Updated "Kontak" navbar item to "Hubungi Kami" dropdown

--- resources/views/templates/user/navbar.blade.php
+++ resources/views/templates/user/navbar.blade.php
@@ -140,10 +140,35 @@
                             </ul>
                         </div>
                     </li>
-                    <li class="w-full sm:w-auto px-6 py-2 sm:px-4 rounded-md font-semibold text-sky-500 text-center hover:bg-sky-500 hover:text-white @if(Request::segment(1) == 'kontak') bg-sky-500 text-white @endif">
-                        <a href="{{ route('kontak') }}">
-                            KONTAK
-                        </a>
+                    <li x-data="{ open: false }"
+                        class="relative"
+                    >
+                        <button class="flex items-center gap-1 cursor-pointer w-full justify-between sm:w-auto px-6 py-2 sm:px-4 rounded-md font-semibold text-sky-500 text-center hover:bg-sky-500 hover:text-white group
+                        @if(Request::segment(1) == 'hubungi-kami') bg-sky-500 text-white @endif"
+                                x-on:click="open = ! open"
+                        >
+                            HUBUNGI KAMI
+                            <svg class="w-3 h-3 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                            </svg>
+                        </button>
+                        <div
+                            class="z-10 hidden w-48 bg-white devide-y rounded-lg shadow absolute top-10"
+                            :class="{ 'hidden': ! open }"
+                        >
+                            <ul class="py-2 text-sm text-gray-600">
+                                <a href="{{ route('kontak')  }}">
+                                    <li class="block text-gray-600 text-sky-500 font-semibold px-4 py-2 hover:bg-sky-500 hover:text-white">
+                                        KONTAK
+                                    </li>
+                                </a>
+                                <a href="">
+                                    <li class="block text-gray-600 text-sky-500 font-semibold px-4 py-2 hover:bg-sky-500 hover:text-white">
+                                        TESTIMONI
+                                    </li>
+                                </a>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
             </div>

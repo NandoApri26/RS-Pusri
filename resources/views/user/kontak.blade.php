8674d3149ed28e10cb0ebe3b48eb8804a146b61e
NandoApri26 <apriando.pratamaa@gmail.com>
2024-05-07 00:58:57 +0700

    Revert "Update page title in kontak.blade.php"
    This reverts commit 1f9f9c4d450ed78d9bda531869e8f72c65557f72.

--- resources/views/user/kontak.blade.php
+++ resources/views/user/kontak.blade.php
@@ -14,7 +14,7 @@
 
     <div class="pt-10">
         <x-tittle
-            title="Kontak"
+            title="Berita"
         />
     </div>
     <div class="px-8 pt-24 space-y-10 sm:px-14 sm:pt-40 lg:pt-28 lg:px-28">
